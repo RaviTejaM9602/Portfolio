2944b4eb75f7d24fe667691be941677b15dda229
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-12-13 17:49:05 +0000

    Upadated the linter errors and remove blur functionality from css

--- mobile.js
+++ mobile.js
@@ -1,18 +1,17 @@
-// close and open menu items 
 const openNav = document.querySelector('.top');
 const closebtn = document.querySelector('.closebtn');
-const myNav_menu = document.getElementById('myNav_mobile');
+const myNavMenu = document.getElementById('myNav_mobile');
 const overlayContent = document.querySelector('.overlay-content');
 
 function openMobileMenu() {
   // creat layout
-  myNav_menu.style.width = '100%';
+  myNavMenu.style.width = '100%';
 }
 
 openNav.addEventListener('click', openMobileMenu);
 
 function closeMobileMenu() {
-  myNav_menu.style.width = '0%';
+  myNavMenu.style.width = '0%';
 }
 
 closebtn.addEventListener('click', closeMobileMenu);
