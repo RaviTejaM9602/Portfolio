f2fe00ebb25feb396afe13a5f18631b776b6123d
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-12-13 17:35:33 +0000

    Added javascript logic and closebtn image

--- mobile.js
+++ mobile.js
@@ -1,8 +1,36 @@
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
+// close and open menu items 
+const openNav = document.querySelector('.top');
+const closebtn = document.querySelector('.closebtn');
+const home = document.querySelector('.main_menu');
+const head_section = document.querySelector('.head-section');
+const myNav_menu = document.getElementById('myNav_mobile');
 
 
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+function openMobileMenu() {
+  // creat layout
+  myNav_menu.style.width = '100%';
+  // add blur in background
+  home.classList.add('blur');
+  head_section.classList.add('blur');
 }
+
+openNav.addEventListener('click', openMobileMenu);
+
+function closeMobileMenu() {
+  //    remove the overlay
+  myNav_menu.style.width = '0%';
+  //    remove blur from background
+  home.classList.remove('blur');
+  head_section.classList.remove('blur');
+}
+
+closebtn.addEventListener('click', closeMobileMenu);
+
+const overlayContent = document.querySelector('.overlay-content');
+
+function closeMobileMenu() {
+  myNav_menu.style.width = '0%';
+  home.classList.remove('blur');
+  head_section.classList.remove('blur');
+}
+overlayContent.addEventListener('click', closeMobileMenu);
