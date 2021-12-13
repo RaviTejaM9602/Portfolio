fb020b4666c4e3ef5bb3c5fd7aec8ed54e710630
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-12-13 17:45:11 +0000

    Corrected Linter Errors and Update the JS code

--- mobile.js
+++ mobile.js
@@ -1,36 +1,19 @@
 // close and open menu items 
 const openNav = document.querySelector('.top');
 const closebtn = document.querySelector('.closebtn');
-const home = document.querySelector('.main_menu');
-const head_section = document.querySelector('.head-section');
 const myNav_menu = document.getElementById('myNav_mobile');
-
+const overlayContent = document.querySelector('.overlay-content');
 
 function openMobileMenu() {
   // creat layout
   myNav_menu.style.width = '100%';
-  // add blur in background
-  home.classList.add('blur');
-  head_section.classList.add('blur');
 }
 
 openNav.addEventListener('click', openMobileMenu);
 
 function closeMobileMenu() {
-  //    remove the overlay
   myNav_menu.style.width = '0%';
-  //    remove blur from background
-  home.classList.remove('blur');
-  head_section.classList.remove('blur');
 }
 
 closebtn.addEventListener('click', closeMobileMenu);
-
-const overlayContent = document.querySelector('.overlay-content');
-
-function closeMobileMenu() {
-  myNav_menu.style.width = '0%';
-  home.classList.remove('blur');
-  head_section.classList.remove('blur');
-}
 overlayContent.addEventListener('click', closeMobileMenu);
