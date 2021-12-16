a88eb409e3378286b57fc890a12c0ab670ca48e3
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-12-16 21:10:10 +0000

    Added logic for Local Storage

---
+++ storage.js
@@ -0,0 +1,20 @@
+const userName = document.querySelector('#username');
+const userEmail = document.querySelector('#email');
+const userMessage = document.querySelector('#message');
+
+function useLocalStorage() {
+  const contactFormData = {
+    name: userName.value,
+    email: userEmail.value,
+    message: userMessage.value,
+  };
+  localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
+}
+
+const formInputs = document.querySelectorAll('input , email , textarea');
+for (let j = 0; j < formInputs.length; j += 1) {
+  formInputs[j].addEventListener('change', () => {
+    useLocalStorage();
+  });
+}
+
