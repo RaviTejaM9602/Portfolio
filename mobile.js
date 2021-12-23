4f4cc37d600d15100376fc6bd3620fad1c84e869
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-12-23 11:04:45 +0000

    Added Logic for Validation of form

--- mobile.js
+++ mobile.js
@@ -196,3 +196,29 @@ close.addEventListener('click', () => {
   document.body.className = 'scroll-show';
   modal.style.display = 'none';
 });
+
+const form = document.querySelector('.input-section');
+const emailMsg = document.querySelector('.email-error');
+
+// Testing Email Functionality and Validating Email
+function testEmail(email) {
+  const emailPattern = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  return emailPattern.test(email);
+}
+function validEmail(emailValue) {
+  if (testEmail(emailValue) === true) {
+    form.submit();
+  } else {
+    emailMsg.innerHTML = 'Please Enter the E-mail Pattern in Lower Case';
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailValue = form.querySelector('#email').value.trim();
+  if (emailValue === '') {
+    emailMsg.innerHTML = 'Email field is required!';
+  } else {
+    validEmail(emailValue);
+  }
+});
